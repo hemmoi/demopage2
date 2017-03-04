f9b2a3fa11cb98574c3be05168b71bd1221043f2
Kimmo Halonen <Kimmo Halonen>
2017-03-04 23:02:37 +0200

    Header image for todo app added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -10,11 +10,10 @@ export class TodoApp extends React.Component {
   render () {
     return (
       <div>
-        <h1 className="page-title">Todo App</h1>
-
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
+              <img src={require('../images/todo.png')} style={{height: 90}} alt="Header for Todo app"/>
               <TodoSearch/>
               <TodoList/>
               <AddTodo/>
